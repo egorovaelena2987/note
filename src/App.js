1397a720293fe20f8aad7f9213075b8fa2a1920d
Elena Egorova <Oleg1@MacBook-Air.local>
2024-04-03 13:54:09 -0500

    change note

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import image from './flower.jpeg';
+import imageTwo from './checklist.jpeg';
+import { Notes } from './Notes';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <img src={ image } width="250px" alt="flower"/>
+      </div>
+      <div className="container">
+      <h1>My Notes</h1>
+      </div>
+      <Notes />
+      <div className="container">
+      <img src={ imageTwo } width="250px" alt="checklist"/>
+      </div>
     </div>
   );
 }
